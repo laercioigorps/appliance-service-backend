1b36b9055c2ce2059b2f3bf536761543939938ec
Laercio Souza <laercioigorps@gmail.com>
2022-04-19 05:28:03 -0300

    test CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,8 @@ DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','still-wildwood-53101.herokuapp.com']
 
+CSRF_TRUSTED_ORIGINS = ['https://powerful-escarpment-12801.herokuapp.com']
+
 # Application definition
 
 INSTALLED_APPS = [
