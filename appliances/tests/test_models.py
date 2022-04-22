c854b8c75f834d8f2b40cbcbc9c2538d37187dec
Laercio Souza <laercioigorps@gmail.com>
2022-04-22 19:12:28 -0300

    added SymptomTests

--- appliances/tests/test_models.py
+++ appliances/tests/test_models.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
-from appliances.models import Brand, Category, Appliance
+from appliances.models import Brand, Category, Appliance, Symptom
 
 # Create your tests here.
+
+
 class BrandTest(TestCase):
     def test_create_brand(self):
         Brand.objects.create(name="Brastemp")
@@ -29,3 +31,43 @@ class ApplianceTest(TestCase):
         self.assertEquals(appliance.model, "BWC11ABANA")
         self.assertEquals(appliance.brand.name, "Brastemp")
         self.assertEquals(appliance.category.name, "Geladeira")
+
+
+class SymptomTest(TestCase):
+
+    def test_create_symptom(self):
+        symptoms_count = Symptom.objects.all().count()
+        self.assertEquals(symptoms_count, 0)
+
+        symptom = Symptom.objects.create(name="Não gela o refrigerador",
+                                         description="A parte de baixo da geladeira está refrigerando abaixo do normal")
+
+        symptoms_count = Symptom.objects.all().count()
+        self.assertEquals(symptoms_count, 1)
+
+        self.assertEquals(symptom.name, "Não gela o refrigerador")
+
+    def test_add_category_to_symptom(self):
+        symptom = Symptom.objects.create(name="Não gela o refrigerador",
+                                         description="A sample description")
+        symptoms_count = symptom.categories.all().count()
+        self.assertEquals(symptoms_count, 0)
+
+        geladeira = Category.objects.create(name="Geladeira")
+        symptom.categories.add(geladeira)
+
+        symptoms_count = symptom.categories.all().count()
+        self.assertEquals(symptoms_count, 1)
+
+    def test_list_symptoms_by_category(self):
+        symptom = Symptom.objects.create(name="Não gela o refrigerador",
+                                         description="A sample description")
+
+        geladeira = Category.objects.create(name="Geladeira")
+        symptoms_by_geladeira_count = geladeira.symptom_set.all().count()
+        self.assertEquals(symptoms_by_geladeira_count, 0)
+
+        symptom.categories.add(geladeira)
+
+        symptoms_by_geladeira_count = geladeira.symptom_set.all().count()
+        self.assertEquals(symptoms_by_geladeira_count, 1)
