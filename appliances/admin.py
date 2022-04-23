78c79f069e9cdfd96eebb4e4080c7938a06aa02a
Laercio Souza <laercioigorps@gmail.com>
2022-04-23 06:38:45 -0300

    added Symptom, Problem, Solution to admin

--- appliances/admin.py
+++ appliances/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from appliances.models import Appliance, Brand, Category
+from appliances.models import Appliance, Brand, Category, Symptom, Problem, Solution
 
 # Register your models here.
 
 admin.site.register(Brand)
 admin.site.register(Category)
 admin.site.register(Appliance)
+
+admin.site.register(Symptom)
+admin.site.register(Problem)
+admin.site.register(Solution)
