0f92b49073a0c1bcb4e5418a443a5cbb3ec9e0f5
Laercio Souza <laercioigorps@gmail.com>
2022-04-23 06:48:00 -0300

    fixed Category verbose_name_plural to categories

--- appliances/models.py
+++ appliances/models.py
@@ -13,6 +13,9 @@ class Brand(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self) -> str:
         return self.name
 
