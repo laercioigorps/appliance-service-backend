fe612cbf12f529121765acae9955f079293dae3f
Laercio Souza <laercioigorps@gmail.com>
2022-04-24 11:38:09 -0300

    added Address model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    number = models.CharField(max_length=10)
+    street = models.CharField(max_length=30)
+    neighborhood = models.CharField(max_length=30)
+    city = models.CharField(max_length=30, null=True)
+    state = models.CharField(max_length=30, null=True)
+    country = models.CharField(max_length=30, default="Brasil")
+    coordinates = models.CharField(max_length=30, null=True)
+    complement = models.CharField(max_length=40, null=True)
+    is_active = models.BooleanField(default=True)
+    type = models.CharField(max_length=30, null=True)
