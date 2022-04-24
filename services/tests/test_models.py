ea11a3cd7925ec9ab5bda99d1ff7985b925bc69e
Laercio Souza <laercioigorps@gmail.com>
2022-04-24 17:05:04 -0300

    created ServiceTest and added test_create_basic_service_without_address

--- services/tests/test_models.py
+++ services/tests/test_models.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from appliances.models import Historic
+
+from profiles.models import Customer, Organization
+from services.models import Service
+
 
 # Create your tests here.
+class ServiceTest(TestCase):
+    def setUp(self) -> None:
+        self.organization = Organization.objects.create()
+        self.customer1 = Customer.objects.create(
+            name="client1", owner=self.organization
+        )
+        self.historic = Historic.objects.create()
+
+    def test_create_basic_service_without_address(self):
+        service_count = Service.objects.all().count()
+        self.assertEqual(service_count, 0)
+
+        service = Service.objects.create(
+            history=self.historic,
+            organization=self.organization,
+            customer=self.customer1,
+        )
+
+        service_count = Service.objects.all().count()
+        self.assertEqual(service_count, 1)
