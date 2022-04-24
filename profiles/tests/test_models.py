121814162aebd11521311506d2e874ef9223b16e
Laercio Souza <laercioigorps@gmail.com>
2022-04-24 11:37:53 -0300

    added test_create_address

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from profiles.models import Address
+
 # Create your tests here.
+class TestAddress(TestCase):
+    def test_create_address(self):
+        address_count = Address.objects.all().count()
+        self.assertEqual(address_count, 0)
+
+        address = Address.objects.create(street="xyz", number="k")
+
+        address_count = Address.objects.all().count()
+        self.assertEqual(address_count, 1)
