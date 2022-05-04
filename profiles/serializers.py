b1a61e0522ba7908797ecc0018d0e0f5c1259005
Laercio Souza <laercioigorps@gmail.com>
2022-05-04 05:44:41 -0300

    created CustomerSerializer modelSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,1 +1,10 @@
+from locale import currency
 from rest_framework import serializers
+
+from profiles.models import Customer
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ["name", "owner", "address"]
