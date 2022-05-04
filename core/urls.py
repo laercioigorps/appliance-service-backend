9ad4d00da8b7f8b85bec4f104d6a0e1d6288dbcb
Laercio Souza <laercioigorps@gmail.com>
2022-05-04 05:48:03 -0300

    added path to profiles.urls

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("profiles/", include("profiles.urls")),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
