9a41d861b0ad7cc4eaf6333076e1cfb3f2863991
Laercio Souza <laercioigorps@gmail.com>
2022-06-20 17:17:01 -0300

    added test_retrieve_customer_address_wich_authenticated_user_does_not_own

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -458,14 +458,25 @@ class TestCustomerAddressDetailView(TestCase):
         stream = io.BytesIO(response.content)
         data = JSONParser().parse(stream)
 
-        self.assertEqual(data['number'], self.address1.number)
-        self.assertEqual(data['street'], self.address1.street)
-        self.assertEqual(data['neighborhood'], self.address1.neighborhood)
+        self.assertEqual(data["number"], self.address1.number)
+        self.assertEqual(data["street"], self.address1.street)
+        self.assertEqual(data["neighborhood"], self.address1.neighborhood)
 
-    
     def test_retrieve_customer_address_with_not_authenticated_user(self):
         client = APIClient()
 
+        response = client.get(
+            reverse(
+                "profiles:customer_address_detail",
+                kwargs={"pk": self.customer1.id, "address_pk": self.address1.id},
+            )
+        )
+
+        self.assertEquals(response.status_code, 403)
+
+    def test_retrieve_customer_address_wich_authenticated_user_does_not_own(self):
+        client = APIClient()
+        client.force_authenticate(self.user1)
         response = client.get(
             reverse(
                 "profiles:customer_address_detail",
