09d202136e757c2a3ac553efd26705c623953058
Laercio Souza <laercioigorps@gmail.com>
2022-06-20 18:05:44 -0300

    added delete method to CustomerAddressDetailView

--- profiles/views.py
+++ profiles/views.py
@@ -104,8 +104,13 @@ class CustomerAddressDetailView(APIView):
         address = get_object_or_404(Address, pk=address_pk)
         self.check_object_permissions(request, address)
         serializer = AddressSerializer(address, request.data)
-        if(serializer.is_valid()):
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, address_pk):
+        address = Address.objects.get(pk=address_pk)
+        address.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
