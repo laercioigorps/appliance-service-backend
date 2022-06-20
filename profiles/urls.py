d57209dcd119d7a7d00e02023338740272966295
Laercio Souza <laercioigorps@gmail.com>
2022-06-20 17:03:17 -0300

    added path to CustomerAddressDetailView named customer_address_detail

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,7 +6,8 @@ app_name = "profiles"
 urlpatterns = [
     path("customers/", views.customer_list_view, name="customer_list"),
     path("customers/<int:pk>", views.CustomerDetailView.as_view(), name="customer_detail"),
-    path("customers/<int:pk>/address/", views.CustomerAddressListView.as_view(), name="customer_address_list")
+    path("customers/<int:pk>/address/", views.CustomerAddressListView.as_view(), name="customer_address_list"),
+    path("customers/<int:pk>/address/<int:address_pk>/", views.CustomerAddressDetailView.as_view(), name="customer_address_detail")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
