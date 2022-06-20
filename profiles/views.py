68a9be10e7857d74f7904b335d89e7529086b88a
Laercio Souza <laercioigorps@gmail.com>
2022-06-20 17:58:33 -0300

    updated CustomerAddressDetailView put method to return HTTP_400 if data not valid

--- profiles/views.py
+++ profiles/views.py
@@ -107,3 +107,5 @@ class CustomerAddressDetailView(APIView):
         if(serializer.is_valid()):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
