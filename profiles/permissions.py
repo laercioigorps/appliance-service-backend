8ac31f8ba4a953886849b6b1b44b7d7413043225
Laercio Souza <laercioigorps@gmail.com>
2022-06-20 17:18:00 -0300

    created IsAddressOwner permission to check if the user organization own the address

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -6,3 +6,9 @@ class IsCustomerOwner(permissions.BasePermission):
         if obj.owner == request.user.profile.org:
             return True
         return False
+
+class IsAddressOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if(obj.customer_set.first().owner == request.user.profile.org):
+            return True
+        return False
