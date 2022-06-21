057b27bee080176814c5a019cf3617dcfe29bcdb
Laercio Souza <laercioigorps@gmail.com>
2022-06-21 16:00:54 -0300

    created CategoryViewTest and test_list_category_with_authenticated_user

--- appliances/tests/test_views.py
+++ appliances/tests/test_views.py
@@ -41,3 +41,26 @@ class BrandViewTest(TestCase):
 
         self.assertEqual(len(data), 2)
 
+
+class CategoryViewTest(TestCase):
+    def setUp(self):
+        self.Category1 = Category.objects.create(name="cat1")
+        self.Category2 = Category.objects.create(name="cat2")
+
+        self.user1 = User.objects.create_user("root1", "email1@exemple.com", "root")
+
+        self.authenticatedClient = APIClient()
+        self.authenticatedClient.force_authenticate(self.user1)
+
+        self.notAuthenticatedClient = APIClient()
+
+    def test_list_category_with_authenticated_user(self):
+        response = self.authenticatedClient.get(
+            reverse("appliances:category_list"), format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+        stream = io.BytesIO(response.content)
+        data = JSONParser().parse(stream)
+
+        self.assertEqual(len(data), 2)
