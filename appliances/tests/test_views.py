6530ab4b4fe54ae29865b45181c45c7c78f02cc4
Laercio Souza <laercioigorps@gmail.com>
2022-06-21 15:57:45 -0300

    added test_list_brand_with_not_authenticated_user

--- appliances/tests/test_views.py
+++ appliances/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from appliances.models import Brand
+from appliances.models import Brand, Category
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 import io
@@ -17,6 +17,8 @@ class BrandViewTest(TestCase):
         self.authenticatedClient = APIClient()
         self.authenticatedClient.force_authenticate(self.user1)
 
+        self.notAuthenticatedClient = APIClient()
+
     def test_list_brand_with_authenticated_user(self):
         response = self.authenticatedClient.get(
             reverse("appliances:brand_list"), format="json"
@@ -27,3 +29,15 @@ class BrandViewTest(TestCase):
         data = JSONParser().parse(stream)
 
         self.assertEqual(len(data), 2)
+
+    def test_list_brand_with_not_authenticated_user(self):
+        response = self.notAuthenticatedClient.get(
+            reverse("appliances:brand_list"), format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+        stream = io.BytesIO(response.content)
+        data = JSONParser().parse(stream)
+
+        self.assertEqual(len(data), 2)
+
