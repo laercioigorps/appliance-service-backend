f70117f5f8ad30b4cb10e2b093bac3d241334c0f
Laercio Souza <laercioigorps@gmail.com>
2022-06-21 16:14:12 -0300

    created ApplianceViewTest and added test_list_appliances_with_authenticated_user

--- appliances/tests/test_views.py
+++ appliances/tests/test_views.py
@@ -1,6 +1,7 @@
+from unicodedata import category
 from django.test import TestCase
 from django.urls import reverse
-from appliances.models import Brand, Category
+from appliances.models import Appliance, Brand, Category
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 import io
@@ -75,3 +76,40 @@ class CategoryViewTest(TestCase):
         data = JSONParser().parse(stream)
 
         self.assertEqual(len(data), 2)
+
+
+class ApplianceViewTest(TestCase):
+    def setUp(self):
+        self.category1 = Category.objects.create(name="cat1")
+        self.category2 = Category.objects.create(name="cat2")
+
+        self.brand1 = Brand.objects.create(name="Brand1")
+        self.brand2 = Brand.objects.create(name="Brand2")
+
+        self.appliance1 = Appliance.objects.create(
+            model="BRW15ABANA", category=self.category1, brand=self.brand1
+        )
+        self.appliance2 = Appliance.objects.create(
+            model="BRW17ABANA", category=self.category2, brand=self.brand1
+        )
+        self.appliance3 = Appliance.objects.create(
+            model="BRW18ABANA", category=self.category2, brand=self.brand2
+        )
+
+        self.user1 = User.objects.create_user("root1", "email1@exemple.com", "root")
+
+        self.authenticatedClient = APIClient()
+        self.authenticatedClient.force_authenticate(self.user1)
+
+        self.notAuthenticatedClient = APIClient()
+
+    def test_list_appliances_with_authenticated_user(self):
+        response = self.authenticatedClient.get(
+            reverse("appliances:appliance_list"), format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+        stream = io.BytesIO(response.content)
+        data = JSONParser().parse(stream)
+
+        self.assertEqual(len(data), 3)
