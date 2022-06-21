cfd5276acdc9885851a1244118d74243de9253fd
Laercio Souza <laercioigorps@gmail.com>
2022-06-21 15:48:15 -0300

    imported important classes and created BrandListView class with get nethod

--- appliances/views.py
+++ appliances/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import BrandSerializer
+from .models import Brand
 
 # Create your views here.
+
+
+class BrandListView(APIView):
+
+    def get(self, request, format=None):
+        brands = Brand.objects.all()
+        serializer = BrandSerializer(brands, many=True)
+        return Response(data = serializer.data)
