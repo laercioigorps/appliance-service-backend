62dd5c3ad3cda96f36b1a5ccbdfadc7dc99feaa4
Laercio Souza <laercioigorps@gmail.com>
2022-06-22 18:45:19 -0300

    created CategoryFactory

--- appliances/tests/factories.py
+++ appliances/tests/factories.py
@@ -3,11 +3,19 @@ import factory
 
 from core.utils.tests.base import faker
 
-from appliances.models import Brand
+from appliances.models import Brand, Category
 
 class BrandFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Brand
+
+    name = factory.LazyAttribute(lambda _: faker.name())
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Category
 
     name = factory.LazyAttribute(lambda _: faker.name())
