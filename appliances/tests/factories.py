7c13f0d95edc17fa6edb9e9b2dba32795a6c6ae3
Laercio Souza <laercioigorps@gmail.com>
2022-06-22 19:16:57 -0300

    created ApplianceFactory

--- appliances/tests/factories.py
+++ appliances/tests/factories.py
@@ -3,14 +3,14 @@ import factory
 
 from core.utils.tests.base import faker
 
-from appliances.models import Brand, Category
+from appliances.models import Appliance, Brand, Category
 
 class BrandFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Brand
 
-    name = factory.LazyAttribute(lambda _: faker.name())
+    name = factory.LazyAttribute(lambda _: faker.company())
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
@@ -18,4 +18,14 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
 
-    name = factory.LazyAttribute(lambda _: faker.name())
+    name = factory.LazyAttribute(lambda _: faker.color_name())
+
+
+class ApplianceFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Appliance
+
+    model = factory.LazyAttribute(lambda _: faker.lexify(text='??????????'))
+    category = factory.SubFactory(CategoryFactory)
+    brand = factory.SubFactory(BrandFactory)
