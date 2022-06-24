098be5d0875c34b812a030905b992775fe36bfc3
Laercio Souza <laercioigorps@gmail.com>
2022-06-24 18:04:47 -0300

    created AddressFactory

--- profiles/tests/factories.py
+++ profiles/tests/factories.py
@@ -1,6 +1,6 @@
 import factory
 
-from profiles.models import Organization
+from profiles.models import Address, Organization
 
 
 from core.utils.tests.base import faker
@@ -13,3 +13,13 @@ class OrganizationFactory(factory.django.DjangoModelFactory):
     name = factory.LazyAttribute(lambda _: faker.company())
 
 
+class AddressFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Address
+
+    number = factory.LazyAttribute(lambda _: faker.building_number())
+    street = factory.LazyAttribute(lambda _: faker.street_name())
+    neighborhood = factory.LazyAttribute(lambda _: faker.city_suffix())
+    city = factory.LazyAttribute(lambda _: faker.city())
+    state = factory.LazyAttribute(lambda _: faker.state())
+    country = factory.LazyAttribute(lambda _: faker.country())
