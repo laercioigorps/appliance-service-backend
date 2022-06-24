f9b6391020759f06e7017aa68b4579a3cfa126bf
Laercio Souza <laercioigorps@gmail.com>
2022-06-24 19:07:05 -0300

    added test_create_service_only_with_required_data

--- service/tests/test_models.py
+++ service/tests/test_models.py
@@ -1,1 +1,25 @@
 from django.test import TestCase
+from service.models import Service
+
+from profiles.tests.factories import (
+    AddressFactory,
+    CustomerFactory,
+    OrganizationFactory,
+)
+from appliances.tests.factories import HistoricFactory
+
+
+class ServiceModelTest(TestCase):
+    def setUp(self):
+        self.org = OrganizationFactory()
+        self.customer = CustomerFactory(
+            address=(AddressFactory(), AddressFactory()), owner=self.org
+        )
+        self.address = self.customer.address.first()
+        self.historic = HistoricFactory(org=self.org)
+
+    def test_create_service_only_with_required_data(self):
+        service_count = Service.objects.all().count()
+        service = Service.objects.create(owner=self.org, historic=self.historic)
+        newServiceCount = Service.objects.all().count()
+        self.assertEqual(newServiceCount, service_count + 1)
