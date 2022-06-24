057d1f06568e996fa779b8ddc0cdded0a3994725
Laercio Souza <laercioigorps@gmail.com>
2022-06-24 19:07:58 -0300

    created basic Serviec model

--- service/models.py
+++ service/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from profiles.models import Customer, Organization
+from appliances.models import Historic
 
 # Create your models here.
+
+class Service(models.Model):
+    owner = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    historic = models.ForeignKey(Historic, on_delete=models.CASCADE)
