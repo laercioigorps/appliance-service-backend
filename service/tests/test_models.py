3e9f45a809b4ac6a886d0d0107122ef7bd2c705d
Laercio Souza <laercioigorps@gmail.com>
2022-06-26 05:53:16 -0300

    added test_add_customer_to_service

--- service/tests/test_models.py
+++ service/tests/test_models.py
@@ -17,9 +17,16 @@ class ServiceModelTest(TestCase):
         )
         self.address = self.customer.address.first()
         self.historic = HistoricFactory(org=self.org)
+        self.service = Service.objects.create(owner=self.org, historic=self.historic)
 
     def test_create_service_only_with_required_data(self):
         service_count = Service.objects.all().count()
         service = Service.objects.create(owner=self.org, historic=self.historic)
         newServiceCount = Service.objects.all().count()
         self.assertEqual(newServiceCount, service_count + 1)
+
+    def test_add_customer_to_service(self):
+        self.assertEqual(self.service.customer, None)
+        self.service.customer = self.customer
+        self.assertEqual(self.service.customer.name, self.customer.name)
+
