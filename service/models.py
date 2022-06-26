8db74bd2ae7cde7ca3f04ba0711000f7e01cdd23
Laercio Souza <laercioigorps@gmail.com>
2022-06-26 05:57:30 -0300

    added address field to service model

--- service/models.py
+++ service/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from profiles.models import Customer, Organization
+from profiles.models import Address, Customer, Organization
 from appliances.models import Historic
 
 # Create your models here.
@@ -8,3 +8,4 @@ class Service(models.Model):
     owner = models.ForeignKey(Organization, on_delete=models.CASCADE)
     historic = models.ForeignKey(Historic, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
