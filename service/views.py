a15fb86d138729f210703259376ed839fddeb830
Laercio Souza <laercioigorps@gmail.com>
2022-06-27 18:34:37 -0300

    created ServiceListView with ServiceListSerializer inside and implemented get method

--- service/views.py
+++ service/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Service
+from rest_framework import serializers
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class ServiceListView(APIView):
+
+    class ServiceListSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Service
+            fields = "__all__"
+            depth = 1
+
+    def get(self, request):
+        services = Service.objects.filter(owner=request.user.profile.org)
+        serializer = self.ServiceListSerializer(services, many=True)
+        return Response(data=serializer.data)
