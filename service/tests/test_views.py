36d741efe5b0aa4408182ca3d6a812d3c5ff7b37
Laercio Souza <laercioigorps@gmail.com>
2022-06-27 18:33:54 -0300

    added test_list_services_by_user_org

--- service/tests/test_views.py
+++ service/tests/test_views.py
@@ -1,1 +1,50 @@
 from django.test import TestCase
+from django.urls import reverse
+from profiles.tests.factories import UserFactory
+from service.models import Service
+from rest_framework.test import APIClient
+import io
+from rest_framework.parsers import JSONParser
+
+from service.tests.factories import ServiceFactory
+
+
+class ServiceViewTest(TestCase):
+    def setUp(self):
+        self.user1 = UserFactory()
+        self.user2 = UserFactory()
+
+        self.user1Client = APIClient()
+        self.user1Client.force_authenticate(user=self.user1)
+
+        self.user2Client = APIClient()
+        self.user2Client.force_authenticate(user=self.user2)
+
+        self.service1 = ServiceFactory(owner=self.user1.profile.org)
+        self.service1.historic.org = self.user1.profile.org
+        self.service1.customer.owner = self.user1.profile.org
+        self.service1.save()
+
+        self.service2 = ServiceFactory(owner=self.user2.profile.org)
+        self.service2.historic.org = self.user2.profile.org
+        self.service2.customer.owner = self.user2.profile.org
+        self.service2.save()
+
+        self.service3 = ServiceFactory(owner=self.user1.profile.org)
+
+    def test_list_services_by_user_org(self):
+        serviceCount = Service.objects.filter(owner=self.user1.profile.org).count()
+        self.assertEqual(serviceCount, 2)
+
+        response = self.user1Client.get(reverse("service:service_list"), format="json")
+
+        self.assertEqual(response.status_code, 200)
+
+        stream = io.BytesIO(response.content)
+        data = JSONParser().parse(stream)
+
+        self.assertEqual(len(data), 2)
+
+        self.assertEqual(data[0]["customer"]["name"], self.service1.customer.name)
+        self.assertEqual(data[0]["address"]["number"], self.service1.address.number)
+        self.assertEqual(data[0]["price"], str(self.service1.price))
