dce42fd35be729f1994dcab7ad34de63647a65ad
Laercio Souza <laercioigorps@gmail.com>
2022-06-27 17:30:02 -0300

    created ServiceFactory

---
+++ service/tests/factories.py
@@ -0,0 +1,23 @@
+import factory
+from core.utils.tests.base import faker
+from service.models import Service
+from profiles.tests.factories import (
+    OrganizationFactory,
+    CustomerFactory,
+    AddressFactory,
+)
+from appliances.tests.factories import HistoricFactory
+from decimal import Decimal
+
+
+class ServiceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Service
+
+    owner = factory.SubFactory(OrganizationFactory)
+    historic = factory.SubFactory(HistoricFactory)
+    customer = factory.SubFactory(CustomerFactory)
+    address = factory.SubFactory(AddressFactory)
+    price = factory.LazyAttribute(
+        lambda _: faker.pydecimal(left_digits=4, right_digits=2)
+    )
