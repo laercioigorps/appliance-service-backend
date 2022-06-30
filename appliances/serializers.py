d125607e38c0d6c0238260230e07a1c7f303f814
Laercio Souza <laercioigorps@gmail.com>
2022-06-30 16:28:02 -0300

    added id into CategorySerializer fields

--- appliances/serializers.py
+++ appliances/serializers.py
@@ -12,7 +12,7 @@ class BrandSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["name"]
+        fields = ["id", "name"]
 
 
 class ApplianceSerializer(serializers.ModelSerializer):
