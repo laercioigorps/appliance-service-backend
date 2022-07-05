67d1196fb507412a829a4d0f5dd327e13ccf3b7b
Laercio Souza <laercioigorps@gmail.com>
2022-07-05 17:42:40 -0300

    updated ServiceDetailView put method to return http 400 if serializer is not valid

--- service/views.py
+++ service/views.py
@@ -63,6 +63,7 @@ class ServiceDetailView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomerServiceListView(APIView):
