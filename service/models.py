ac1accb85fc19c593ff831ad128bf693d5d56c6d
Laercio Souza <laercioigorps@gmail.com>
2022-07-14 19:03:08 -0300

    updated service model to use start_date datetime.date.today as default

--- service/models.py
+++ service/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from profiles.models import Address, Customer, Organization
 from appliances.models import Historic
+from datetime import date
 
 # Create your models here.
+
 
 class Service(models.Model):
     owner = models.ForeignKey(Organization, on_delete=models.CASCADE)
     historic = models.ForeignKey(Historic, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
-    start_date = models.DateField(auto_now_add=True)
+    start_date = models.DateField(default=date.today())
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
