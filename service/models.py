943be912bd5b8ee2bb52011d26caa5ec29d39035
Laercio Souza <laercioigorps@gmail.com>
2022-07-14 19:07:38 -0300

    added end_date to service model

--- service/models.py
+++ service/models.py
@@ -12,4 +12,5 @@ class Service(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
     start_date = models.DateField(default=date.today())
+    end_date = models.DateField(null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
