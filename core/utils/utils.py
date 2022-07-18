d04847f629a453982c2dfff31db3ea1e609e02fd
Laercio Souza <laercioigorps@gmail.com>
2022-07-18 19:47:00 -0300

    added function concatenateLists

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -1,10 +1,16 @@
-
 def getDisctionaryOfLists(list):
     dict = {}
     for d in list:
         for key, value in d.items():
-            if(dict.get(key)):
+            if dict.get(key):
                 dict[key].append(value)
             else:
                 dict[key] = [value]
     return dict
+
+
+def concatenateLists(list1, list2, separator):
+    newList = []
+    for i in range(len(list1)):
+        newList.append("%s%s%s" % (list1[i], separator, list2[i]))
+    return newList
