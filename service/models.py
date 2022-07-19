6f73d8f02bb0e1a1b8c9130a374be492762bdebc
Laercio Souza <laercioigorps@gmail.com>
2022-07-19 18:28:43 -0300

    added status field to Service model

--- service/models.py
+++ service/models.py
@@ -6,6 +6,13 @@ from datetime import date
 # Create your models here.
 
 
+class Status(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=70)
+    is_conclusive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+
 class Service(models.Model):
     owner = models.ForeignKey(Organization, on_delete=models.CASCADE)
     historic = models.ForeignKey(Historic, on_delete=models.CASCADE)
@@ -14,10 +21,4 @@ class Service(models.Model):
     start_date = models.DateField(default=date.today())
     end_date = models.DateField(null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-
-
-class Status(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=70)
-    is_conclusive = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True)
