f45d61e0617f4c891c80a670b20670c03da3fbfa
Laercio Souza <laercioigorps@gmail.com>
2022-07-20 18:38:41 -0300

    added service to admin site

--- service/admin.py
+++ service/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from service.models import Status
+from service.models import Service, Status
 
 # Register your models here.
 
 admin.site.register(Status)
+admin.site.register(Service)
