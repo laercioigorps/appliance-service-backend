fa14fade8dec69550368be8df97e16da0d1aa7b0
Laercio Souza <laercioigorps@gmail.com>
2022-07-27 17:59:51 -0300

    updated SampleDataCreation init to set organization to None

--- service/services.py
+++ service/services.py
@@ -5,6 +5,7 @@ class SampleDataCreation:
     def __init__(self):
         self.customers = []
         self.statuses = []
+        self.organization = None
 
     def updateStatuses(self):
         self.statuses = Status.objects.all()
