b168c5c010cb9e9f197f880af7c17fecac3b715f
Laercio Souza <laercioigorps@gmail.com>
2022-07-27 17:35:37 -0300

    implementhed updateStatuses method

--- service/services.py
+++ service/services.py
@@ -1,4 +1,10 @@
+from service.models import Status
+
+
 class SampleDataCreation:
     def __init__(self):
         self.customers = []
         self.statuses = []
+
+    def updateStatuses(self):
+        self.statuses = Status.objects.all()
