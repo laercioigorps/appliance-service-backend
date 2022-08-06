ab8362db986526faf379eea4e7e0ca946a54fc1c
Laercio Souza <laercioigorps@gmail.com>
2022-08-06 14:55:35 -0300

    updated list_customers annd renamed to test_list_3_customers_limited_by_10 in TestCustomerView

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -43,7 +43,7 @@ class TestCustomerView(TestCase):
         customer_count = Customer.objects.all().count()
         self.assertEqual(customer_count, 5)
 
-    def test_list_customer_by_user_organization(self):
+    def test_list_3_customers_limited_by_10(self):
         customer_owned_by_user2_organization = Customer.objects.filter(
             owner=self.user2.profile.org
         ).count()
@@ -51,13 +51,15 @@ class TestCustomerView(TestCase):
 
         client = APIClient()
         client.force_authenticate(user=self.user2)
-        response = client.get(reverse("profiles:customer_list"))
+        response = client.get(
+            "%s?limit=10&offset=0" % reverse("profiles:customer_list")
+        )
         self.assertEqual(response.status_code, 200)
 
         stream = io.BytesIO(response.content)
         data = JSONParser().parse(stream)
 
-        self.assertEqual(len(data), 3)
+        self.assertEqual(len(data["results"]), 3)
 
 
 class TestCustomerDetailView(TestCase):
